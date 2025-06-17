7ccba4a5130d9781518c0684fc4dfc3bfcd41064
Pedrovisk19 <pepeob12@gmail.com>
2025-06-17 19:21:24 -0300

    AF-0007 : adicionando rota e verificação de token válido

--- src/app/new-password-reset/page.tsx
+++ src/app/new-password-reset/page.tsx
@@ -9,32 +9,55 @@ import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { ToastContainer, toast } from 'react-toastify'
 import { z } from 'zod'
-
+import { useSearchParams } from 'next/navigation'
 import axios from 'axios'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './reset-password.css'
 
 const schema = z.object({
-  password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
-})
+  password: z.string()
+    .min(8, 'Senha deve ter pelo menos 8 caracteres')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/,
+      'A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial'
+    ),
+  passwordConfirmation: z.string(),
+}).refine((data) => data.password === data.passwordConfirmation, {
+  message: 'As senhas não coincidem',
+  path: ['passwordConfirmation'],
+});
+
+
 
 type FormData = z.infer<typeof schema>
 
 export default function NewPasswordResetPage() {
 
+  const searchParams = useSearchParams();
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(schema),
   })
   const [loading, setLoading] = useState(false)
 
-  const router = useRouter()
+  const router = useRouter();
+
+  const verifyToken = async () => {
+    await axios.get('http://localhost:3002/email/verify-token');
+  };
+
+  useEffect(() => {
+    verifyToken();
+  }, [])
 
   const resetPassword = async (data: FormData) => {
     setLoading(true)
     try {
-      await axios.post('http://localhost:3002/reset/update-password', {
-        password: data.password,
+      const token = searchParams.get('token_pass');
+
+      await axios.post('http://localhost:3002/auth/update-password', {
+        password: data.passwordConfirmation,
+        token: token
       });
 
       toast.success('Senha redefinida com sucesso!', {
@@ -60,11 +83,25 @@ export default function NewPasswordResetPage() {
   return (
     <div className="form-reset-password  flex column justify-center items-center h-screen">
       <Code size="lg" variant="solid" colorPalette="purple">Reset Password</Code>
-      <Input placeholder="senha" {...register('password')} variant="subtle" />
+      <Input placeholder="senha" {...register('password')} variant="subtle" type='password' />
+      <Input placeholder="confirmação de senha" {...register('passwordConfirmation')} variant="subtle" type='password' />
 
-      <Button onClick={handleSubmit(resetPassword)}>
+      <Button
+        onClick={handleSubmit(resetPassword, (formErrors) => {
+          const messages = Object.values(formErrors).map((err) => err?.message);
+          messages.forEach((msg) => {
+            toast.error(msg, {
+              position: 'top-right',
+              autoClose: 3000,
+              theme: 'dark',
+            });
+          });
+        })}
+      >
         Alterar Senha
       </Button>
+
+
       <Link href="/" ></Link>
       <ToastContainer />
 
